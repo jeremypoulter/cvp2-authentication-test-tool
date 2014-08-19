9c8f718958908a95a87c37ab40469b3d91886499
Brendan Long <b.long@cablelabs.com>
2014-08-19 16:43:50 -0600

    Make output easier to understand.

--- catt.py
+++ catt.py
@@ -55,10 +55,12 @@ class Test(object):
 
     def run(self, debug, host, port, path, openssl, log_path):
         filename = os.path.join(log_path, "{}.log".format(self.log_name))
-        print("Testing: {} (log: {})".format(self.name, filename))
+        print("Testing: {} (log: {})".format(self.name,
+            os.path.relpath(filename)))
 
         if self.key is None:
-            print("SKIP: Required key not given as argument\n")
+            print("Required key not given as argument")
+            print("TEST SKIPPED\n")
             return
 
         fail = False
@@ -84,27 +86,33 @@ class Test(object):
                 print("ERROR: OpenSSL does not support -dtcp argument in s_client. Make sure you're using the CVP2 OpenSSL (https://community.cablelabs.com/wiki/display/CBLCVP2/Openssl+Implementation)\n")
                 return
 
-            # Check output to make sure the CVP2 bit was set on the server side
-            if not "CVP2_DTCIP_VerifyRemoteCert(): CVP2 bit set" in output:
-                fail = True
-                print("FAIL: CVP2 bit not set in remote cert")
-            # TODO: Check for "CVP2 bit not set" or whatever the message is
-            # once we have a failure case
-            elif debug:
-                print("Debug: CVP2 bit is set")
-
             # Make sure the client attempts to continue the connection, even
             # if the server authentication failed
             if not "Inside DTCPIPAuth_SignData" in output:
-                print("ERROR: OpenSSL is setup incorrectly. Make sure validate_dtcp_suppdata() in s_client.c *always* returns 0, including in error conditions (note: this is insecure and should only be used for the test tool)\n")
+                print("ERROR: OpenSSL is setup incorrectly. Make sure validate_dtcp_suppdata() in s_client.c ALWAYS returns 0, including in error conditions (note: this is insecure and should only be used for the test tool)\n")
                 return
 
-        if return_code != 0 and self.should_succeed:
-            print("FAIL: Connection failed when it should have succeeded")
-        elif return_code == 0 and not self.should_succeed:
-            print("FAIL: Connection succeeded when it should have failed")
-        elif not fail:
-            print("PASS")
+            # Check output to make sure the CVP2 bit was set on the server side
+            if not "CVP2_DTCIP_VerifyRemoteCert(): CVP2 bit set" in output:
+                fail = True
+                print("CVP2 bit is NOT set in remote certificate")
+            # TODO: Check for "CVP2 bit not set" or whatever the message is
+            # once we have a failure case
+            else:
+                print("CVP2 bit is set in remote certificate")
+
+        if return_code != 0:
+            print("Connection failed")
+            if self.should_succeed:
+                fail = True
+        else:
+            print("Connection succeeded")
+            if not self.should_succeed:
+                fail = True
+        if fail:
+            print("TEST FAILED")
+        else:
+            print("TEST SUCCEEDED")
         print()
 
 
